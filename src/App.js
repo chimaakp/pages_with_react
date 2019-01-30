ed784832b0b32ea8b0fec28c55e74e2b704494b6
Chima Akparanta <chimaakp@gmail.com>
2019-01-29 21:54:00 -0500

    starting da process

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './anchorlogo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,17 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ul className="App-link">
+            <li>Helen Akparanta, Administrator</li>
+            <li>email</li>
+            <li>phone number</li>
+          </ul>
         </header>
       </div>
     );
@@ -26,3 +20,11 @@ class App extends Component {
 }
 
 export default App;
+
+export class Topbar extends Component {
+  render(){
+    return{
+
+    }
+  }
+}
